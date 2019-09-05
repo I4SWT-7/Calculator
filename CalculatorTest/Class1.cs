447514f6f16e2c7c27b2df60a76aad5f13d832f8
MHJespersen <mathiasholsko@hotmail.com>
2019-09-05 13:04:20 +0200

    Adding unit tests

--- CalculatorTest/Class1.cs
+++ CalculatorTest/Class1.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-
-namespace CalculatorTest
-{
-    [TestFixture]
-    public class Class1
-    {
-        
-
-    }
-}
+﻿
